eebd73979bc8b1b5bc8de281ba6a4a2f3d5f84af
Mohammed4371 <mohammedibrahim4371@gmail.com>
2022-06-20 20:01:06 +0530

    javascript practice

---
+++ script.js
@@ -0,0 +1,24 @@
+//string//
+var car="Rolls royce"
+console.log(typeof car,car);
+//number//
+var a=5
+console.log(typeof a,a);
+//boolean//
+var available=true
+console.log(typeof available, available);
+//null//
+var owner=null
+console.log(typeof owner,owner);
+//undefined//
+var condition= undefined
+console.log(typeof condition,condition);
+//symbol//
+var vol=Symbol()
+console.log(typeof vol,vol);
+//corceian//
+var a=5
+var b="5"
+var sum=(a+b)
+console.log(typeof sum,sum);
+
